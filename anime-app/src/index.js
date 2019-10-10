ef9ebe4a6c1733124c543e8dcf733e644b62bb4b
strimnal32246 <susan.trimnal@icdccollege.edu>
2019-10-10 16:23:59 -0400

    trying to fix bug

--- anime-app/src/index.js
+++ anime-app/src/index.js
@@ -4,7 +4,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
-import rootReducer from "../reducer/animeManga.js";
+import rootReducer from "./reducer/animeManga.js";
 
 import './App.css';
 
